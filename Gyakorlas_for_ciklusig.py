0b418094e472a18cc653e4b2efd3e2278183b1d5
Gondi Zsolt <gondi.zsolt@diak.szbi-pg.hu>
2024-11-04 10:44:36 +0100

    ign

---
+++ Gyakorlas_for_ciklusig.py
@@ -0,0 +1,107 @@
+"""1. Egyszerű számbekérő
+Kérj be egy számot a felhasználótól, és döntsd el, hogy páros vagy páratlan. Írd ki az eredményt!”””
+"""
+
+
+# szam = int(input("Adj meg egy számot!"))
+# if szam % 2 == 0:
+#     print("A számod páros.")
+# else:
+#     print("A számod páratlan.")
+
+"""
+2. Összegszámítás
+Kérj be egy egész számot, és számítsd ki az 1-től a megadott számig terjedő egész számok összegét.
+"""
+
+
+# szam = int(input("Adj meg egy egész számot."))
+
+"""
+3. Számok listázása és összegzése
+Írj egy programot, amely bekér  n számot a felhasználótól, majd egy while ciklussal megkérdezi a felhasználót, hogy szeretne-e újabb számot megadni. Addig folytassa a program a számok bekérését, amíg a felhasználó igennel válaszol. A program végén jelenítse meg a bekért számok összegét.
+b) jelenítse meg a bekért számokat (lista használata)
+"""
+
+
+# folytat = True
+# while folytat:
+#     szam = int(input("ADJÁL SZÁMOT GENYO"))
+#     kerdes = input("még? (i/n)")
+#     if kerdes == "n":
+#         folytat = False
+
+
+"""
+4. Két szám közötti számok
+Kérj be két számot a felhasználótól (a és b). Írasd ki az összes számot a és b között.
+b) Ha a nagyobb, mint b, akkor csökkenő sorrendben írasd ki őket.
+"""
+
+
+# szam1 = int(input("ADJÁL SZÁM"))
+# szam2 = int(input("ADJÁL SZÁM MEGIN"))
+
+
+"""
+5. Egyszerű jelszókérés
+Készíts egy programot, amely egy előre meghatározott jelszót vár el a felhasználótól. A program addig kérdez, amíg a helyes jelszót meg nem adják. Ha eltalálja a jelszót, jelenjen meg egy üzenet, hogy „Sikeres belépés”.
+"""
+
+
+# ujra = True
+# while ujra:
+#     szo = input("ADNI JELSZÓ MOST")
+#     if szo == "cignig12":
+#         ujra = False
+# print("Sikeres belépés")
+
+
+"""
+6. Szorzótábla
+Írasd ki egy adott szám szorzótábláját 1-től 10-ig. Például, ha a felhasználó 5-öt ad meg, akkor az eredmény legyen:
+5 x 1 = 5
+5 x 2 = 10
+...
+5 x 10 = 50
+"""
+# szam = int(input("ADNI SZÁM ÉS ÉN KIÍR SZORZÓTÁBLA"))
+# szorzo = 0
+# for i in range(1, 11):
+#     szorzo = szorzo + 1
+#     i *= szam
+#     print(f"{szam} x {szorzo} ={i}")
+
+
+"""
+7. Maximum keresés lista elemeiben
+Készíts egy programot, amely bekér 5 számot a felhasználótól, és kiírja a legnagyobb számot. A programban használj egy for ciklust a számok bekérésére, és egy if feltételt a legnagyobb szám megkeresésére.
+"""
+
+
+
+
+"""
+8. Prímszám ellenőrzés
+Kérj be egy számot, és döntsd el, hogy prímszám-e vagy sem. A program akkor jelezze, ha prímszámot talált, és akkor is, ha nem az.
+"""
+
+
+
+
+"""
+9. Piramis rajzolása csillagokkal
+Kérj be egy számot, amely megadja a piramis magasságát, majd rajzolj ki egy csillagpiramist ennek megfelelően. Például egy 5 magas piramis:
+    *
+   ***
+  *****
+ *******
+*********
+"""
+
+
+
+"""
+10. Szorzótábla mátrix formában
+Készíts egy programot, amely kiírja az 1-től 10-ig terjedő szorzótáblát egy 10x10-es mátrix formájában. Minden sor egy-egy i értéket képviseljen, minden oszlop pedig egy j értéket, és az i * j szorzatot jelenítse meg.
+"""
